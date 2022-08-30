e5d93a004915d4104ab55414929641f5551869fd
Grace Wang <t4_23@yahoo.com.tw>
2022-08-30 22:11:04 +0800

    Update 2022-08-30T14:11:01.055Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -27,7 +27,7 @@ AOS.init({
   // values from 0 to 3000, with step 50ms
   easing: 'ease',
   // default easing for AOS animations
-  once: true,
+  once: false,
   // whether animation should happen only once - while scrolling down
   mirror: false,
   // whether elements should animate out while scrolling past them
